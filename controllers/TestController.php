c37ac8e4222cb7258033c95d103e1a09c6c04b5d
allengeer <allengeer@gmail.com>
2014-03-29 11:37:23 -0500

    added forms

--- controllers/TestController.php
+++ controllers/TestController.php
@@ -21,4 +21,23 @@ class CLT_QBCSV_TestController extends Mage_Adminhtml_Controller_Action {
         $this->getLayout()->getBlock('content')->append($this->getLayout()->createBlock('qbcsv/adminhtml_edit_params'));
         $this->renderLayout();
     }
+
+    public function exportAction()
+    {
+//        $this->_setActiveMenu('qbexportmenu/qbexport');
+
+        $this->loadLayout();
+        $post = $this->getRequest()->getPost();
+        
+        $this->_addContent($this->getLayout()
+            ->createBlock('core/text', 'example-block')
+            ->setText('<h1>'.$post['startDate'] . ' - ' . $post['endDate'] .'</h1>'));
+        $this->_addContent($this->getLayout()->createBlock('qbcsv/adminhtml_edit_params'));
+        $this->renderLayout();
+    }
+    protected function _addContent(Mage_Core_Block_Abstract $block)
+    {
+        $this->getLayout()->getBlock('content')->append($block);
+        return $this;
+    }
 }
