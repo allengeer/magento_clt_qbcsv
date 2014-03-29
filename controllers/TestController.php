3f9eb9e79eaa42e48c34d4f711fbd1690f6602fb
allengeer <allengeer@gmail.com>
2014-03-29 11:03:54 -0500

    added forms

--- controllers/TestController.php
+++ controllers/TestController.php
@@ -16,7 +16,7 @@ class CLT_QBCSV_TestController extends Mage_Adminhtml_Controller_Action {
 
     public function editAction()
     {
-        $this->_setActiveMenu('qbexportmenu/qbexport');
+//        $this->_setActiveMenu('qbexportmenu/qbexport');
         $this->loadLayout();
 //        $this->getResponse()->setBody($this->getLayout()->createBlock('qbcsv/adminhtml_edit_params')->toHtml());
         $this->renderLayout();
