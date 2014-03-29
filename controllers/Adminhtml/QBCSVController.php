6b88009b44634e22c587b1b886c76699447eb247
allengeer <allengeer@gmail.com>
2014-03-29 08:03:45 -0500

    baseline

---
+++ controllers/Adminhtml/QBCSVController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ageer
+ * Date: 3/29/14
+ * Time: 7:56 AM
+ */
+
+class CLT_QBCSV_IndexController extends Mage_Adminhtml_Controller_Action {
+    public function indexAction()
+    {
+        $this->loadLayout()->renderLayout();
+    }
+
+}
